86169ba4a5bdf127640705379dea7e2e194ae13e
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-07-05 14:28:00 +0100

    task5 corrected

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,10 +11,23 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
-		return (0);
-	
-	if 
-	i = _sqrt_recursion(n - 1);
-	if (n == (i - 1) * (i - 1))
+	return (recursion(n, 0));
 }
+
+/**
+ * recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (recursion(n, i + 1));
+}
+
