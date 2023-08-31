a73ed32606877b871a712fe04d487ab18b1216fc
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-08-31 20:48:34 +0100

    0x14-bit_manipulation Created

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i = 1;
+	char *s = (char *)&i;
+
+	return ((int)*s);
+}
