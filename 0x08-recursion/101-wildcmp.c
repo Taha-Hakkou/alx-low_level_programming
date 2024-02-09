64c8f7c63c164076a36cc26e463a342cf265556c
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-09 18:35:56 +0100

    0x08 101-wildcmp update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * wildcmp - compares 2 strings
  * @s1: first string
@@ -8,33 +9,27 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	else if (*s1 == '\0')
-	{
-		if (*s2 == '*')
-			return (wildcmp(s1, ++s2));
-		return (0);
-	}
-	else if (*s2 == '\0')
-	{
-		if (*s1 == '*')
-			return (wildcmp(++s1, s2));
-		return (0);
-	}
-	else if (*s1 == '*')
-	{
-		if (*s2 == '*')
-			return (wildcmp(s1, ++s2) || wildcmp(++s1, s2));
-		return (wildcmp(s1, ++s2));
-	}
-	else if (*s2 == '*')
-		return (wildcmp(++s1, s2));
-	else
-	{
-		if (*s1 == *s2)
-			return (wildcmp(++s1, ++s2));
-		else
+	char *str1 = s1, *str2 = s2;
+
+	while (*str1 != '\0' && *str2 != '\0' && *str2 != '*')
+		if (*(str1++) != *(str2++))
 			return (0);
+	if (*str1 == '\0')
+	{
+		while (*str2 != '\0')
+			if (*(str2++) != '*')
+				return (0);
+		return (1);
 	}
+	if (*str2 == '\0')
+	{
+		if (*str1 == '\0')
+			return (1);
+		return (0);
+	}
+	do {
+		if (wildcmp(str1, str2 + 1))
+			return (1);
+	} while (*(str1++) != '\0');
+	return (0);
 }
